273ee513075f0f4d20f2e6644840e12e4214ae29
Vivek Shah <bonii@kernelspace.in>
2016-05-02 14:40:41 +0200

    Cleaned up the code to use the compile time parameters

--- src/hash_functions/mod_hash.h
+++ src/hash_functions/mod_hash.h
@@ -2,20 +2,16 @@
 #define mod_hash_h
 
 #include <cstdint>
+#include "abstract_hash.h"
 
 namespace dbindex {
     template<typename value_t = std::uint32_t, std::uint32_t _mod_value = 2>
     class mod_hash: public abstract_hash<value_t> {
-    private:
-        std::uint32_t mod_value;
     public:
-        mod_hash() {
-            mod_value = _mod_value;
-        }
         value_t get_hash(const std::string& key) override {
-            const std::uint32_t ukey = std::atoi(key.c_str());
+            const value_t ukey = std::atoi(key.c_str());
             // std::cout << "ModHash: " << skey << ", " << ukey << std::endl;
-            return ukey % mod_value;
+            return ukey % _mod_value;
         }
     };
 }
