3a754b42ffbce577df60abb41d8a66dcc714f000
KrimsKrams13 <jakob.hanehoj@gmail.com>
2016-06-01 09:36:35 +0200

    Changes to order of tests

--- test/ycsb_all.sh
+++ test/ycsb_all.sh
@@ -1,6 +1,6 @@
-for wl in workload_a workload_b workload_c workload_d workload_f;
+for wl in workload_b workload_c workload_d workload_f workload_a;
 do
-	for hf in 1 2;
+	for hf in 2 1;
 	do
 		for hi in 0 1;
 		do
